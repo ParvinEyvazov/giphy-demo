82564eda8fc73594396dcb1e6f7061cd80a9bcc1
ParvinEyvazov <pervin.eyvazov1999@gmail.com>
2021-03-01 03:31:11 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ViewChild } from '@angular/core';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'giphy-demo';
+export class AppComponent implements OnInit {
+  @ViewChild('input') input;
+
+  API_KEY = 'OseukRU47oPpVHxynLs6Ma9x6H3CI8Yw';
+  search_text: string;
+  gif_data;
+  sticker_data;
+  images: string[] = [];
+
+  gifs: string[] = [];
+  stickers: string[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngAfterViewInit() {
+    this.input.update.pipe(debounceTime(500)).subscribe((value) => {
+      this.findGifs(value);
+    });
+  }
+
+  ngOnInit(): void {}
+
+  findGifs(search_text) {
+    if (search_text) {
+      this.getGifs(search_text)
+        .toPromise()
+        .then((data) => {
+          this.gif_data = data;
+          this.showGifs(this.gif_data);
+        });
+      this.getStickers(search_text)
+        .toPromise()
+        .then((data) => {
+          this.sticker_data = data;
+          this.showStickers(this.sticker_data);
+        });
+    }
+  }
+
+  getGifs(search_text) {
+    let url = `https://api.giphy.com/v1/gifs/search?api_key=${this.API_KEY}&q=${search_text}`;
+    return this.http.get(url);
+  }
+
+  getStickers(search_text) {
+    let url = `https://api.giphy.com/v1/stickers/search?api_key=${this.API_KEY}&q=${search_text}`;
+    return this.http.get(url);
+  }
+
+  showGifs(gif_data) {
+    this.gifs.length = 0;
+    let gif_array = gif_data.data;
+    gif_array.forEach((element) => {
+      this.gifs.push(element.images.original.url);
+    });
+  }
+
+  showStickers(sticker_data) {
+    this.stickers.length = 0;
+    let gif_array = sticker_data.data;
+    gif_array.forEach((element) => {
+      this.stickers.push(element.images.original.url);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { TextFieldModule } from '@angular/cdk/text-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    TextFieldModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
